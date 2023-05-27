714651784de8ac6ba11790a832aa2b472e8eb108
断简残篇 <11097339+jipeng03@user.noreply.gitee.com>
2023-05-27 13:18:45 +0800

    first commit!

---
+++ src/cn/baisee/HelloWorld.java
@@ -0,0 +1,13 @@
+package cn.baisee;
+
+/**
+ * description: HelloWorld
+ * date: 2023/5/27 13:14
+ * author: 沐歆
+ * version: 1.0
+ */
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello Git");
+    }
+}
